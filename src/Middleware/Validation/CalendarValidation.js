7e8f1d552bc20dcc66b603b30ee548d95de42741
vianviet <vianviet2502@gmail.com>
2022-06-01 21:37:26 +0700

    Add calendar validation

---
+++ src/Middleware/Validation/CalendarValidation.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+
+function addCalendarValidation(req, res, next) {
+  const schema = Joi.object({
+    title: Joi.string().max(6).required(),
+    start: Joi.string().required(),
+    end: Joi.string().required(),
+    phone: Joi.string().max(11).min(9).required(),
+    message: Joi.string().required(),
+  });
+  validateRequest(req, res, next, schema);
+}
+function validateRequest(req, res, next, schema) {
+  const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  };
+  const { error, value } = schema.validate(req.body, options);
+  if (error) {
+    res.status(406).send(
+      error.details.reduce((result, item) => {
+        result[item.path[0]] = item.message;
+        return result;
+      }, {})
+    );
+  } else {
+    req.body = value;
+    next();
+  }
+}
+module.exports = { addCalendarValidation };
--- src/Router/Calendar.js
+++ src/Router/Calendar.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const { FindAll, AddCalendar, findAllController, addCalendarController } = require('../Controller/Calendar');
-const calendarRoute = express.Router()
+const express = require("express");
+const {
+  FindAll,
+  AddCalendar,
+  findAllController,
+  addCalendarController,
+} = require("../Controller/Calendar");
+const {
+  addCalendarValidation,
+} = require("../Middleware/Validation/CalendarValidation");
+const calendarRoute = express.Router();
 
-
-calendarRoute.get("/", async(req, res) => {
-    findAllController(req, res)
+calendarRoute.get("/", async (req, res) => {
+  findAllController(req, res);
 });
 
-calendarRoute.post("/", async(req, res) => {
-    addCalendarController(req, res);
+calendarRoute.post("/", addCalendarValidation, async (req, res) => {
+  addCalendarController(req, res);
 });
 
-module.exports = calendarRoute
+module.exports = calendarRoute;
